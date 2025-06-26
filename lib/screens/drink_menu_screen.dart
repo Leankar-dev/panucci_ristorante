10296f38bba720d66420481a7ea5ccd38643be38
Leandro <l.silva01@uol.com.br>
2025-06-26 21:13:39 +0100

    criacao drink_menu_screen com GridView.builder

--- lib/screens/drink_menu_screen.dart
+++ lib/screens/drink_menu_screen.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:panucci_ristorante/core/cardapio.dart';
+import 'package:panucci_ristorante/core/widgets/drink_item.dart';
 
 class DrinkMenuScreen extends StatelessWidget {
+  final List items = drinks;
   const DrinkMenuScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('drink')),
-      body: Container(),
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 0.75,
+      ),
+      itemBuilder: (context, index) {
+        return DrinkItem(
+          imageURI: items[index]['image'] ?? '',
+          itemTitle: items[index]['name'] ?? '',
+          itemPrice: items[index]['price'] ?? '',
+        );
+      },
+      itemCount: items.length,
     );
   }
 }
